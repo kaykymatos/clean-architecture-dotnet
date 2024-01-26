83800901d0da7a01cb0e7354e16ddec9181dc4cb
Kayky Matos Santana <kayky7277@gmail.com>
2024-01-26 19:21:09 -0300

    pages and actions done

--- CleanArchProject.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
+++ CleanArchProject.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -11,6 +11,7 @@ namespace CleanArchProject.Infra.Data.EntitiesConfiguration
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
 
+
             builder.HasData(
                 new Category(1, "Materiais Escolares"),
                 new Category(2, "Eletrônicos"),
